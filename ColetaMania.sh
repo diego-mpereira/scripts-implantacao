0ba3040b14a1f6dd7daa16b498e1cfe7b6cb6896
Diego Meirinho C Pereira Gonçalves <137406825+diego-mpereira@users.noreply.github.com>
2023-08-24 18:18:16 -0300

    Create ColetaMania.sh

---
+++ ColetaMania.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+addr=$(cat /sys/class/net/*/address | head -n 1) && addr=$(echo $addr | sed 's/://g')
+wget https://raw.githubusercontent.com/diego-mpereira/scripts-implantacao/main/FidelidadeManiaCpfPreco.sh
+chmod +x FidelidadeManiaCpfPreco.sh
+./FidelidadeManiaCpfPreco.sh /opt/videosoft/vs-os-interface/log/vs-fast*.log
+rm FidelidadeManiaCpfPreco.sh
+mv newlog.log $addr.log
+curl -u 'diego:Stonesour159!@' -T $addr.log sftp://diegovps.vps-kinghost.net/home/diego/
