462feb53c6b0d82abcb01ad4f06411f43792a3c8
Diego Meirinho C Pereira Gonçalves <137406825+diego-mpereira@users.noreply.github.com>
2023-08-25 10:05:34 -0300

    Update ColetaMania.sh

--- ColetaMania.sh
+++ ColetaMania.sh
@@ -17,7 +17,7 @@ sed 's/^[ \t]*//' newlog.log | sed -E 'N;/^(.*)\n\1/!P;D' >>newlog3.log && mv ne
 awk '!a[$0]++' newlog.log >>newlog3.log && mv newlog3.log newlog.log
 grep -A1 "document\|score" newlog.log >>newlog3.log && mv newlog3.log newlog.log
 done
-awk '!a[$0]++' manialogfood.log >>$addr-food.log
+mv manialogfood.log $addr-food.log
 rm manialogfood.log
 # Envio do dos logs processados para o servidor remoto
 curl -u 'diego:V$V$V$3363' -T $addr-food.log --insecure sftp://diegovps.vps-kinghost.net/home/diego/manialogs/
