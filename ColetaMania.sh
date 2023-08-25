5e9052a9a0df5d57d0081cd13369cfd5ace08243
Diego Meirinho C Pereira Gonçalves <137406825+diego-mpereira@users.noreply.github.com>
2023-08-25 13:20:26 -0300

    Update ColetaMania.sh

--- ColetaMania.sh
+++ ColetaMania.sh
@@ -2,8 +2,12 @@
 # MAC Addr como unique id e nome do log que será enviado
 addr=$(cat /sys/class/net/*/address | head -n 1) && addr=$(echo $addr | sed 's/://g')
 # Processamento de logs do Food
-for i in /opt/videosoft/vs-os-interface/log/vs-fast*.log; do
-sed '/^2/d' $i >>newlog.log 
+#for i in /opt/videosoft/vs-os-interface/log/vs-fast*.log; do
+#sed '/^2/d' $i >>newlog.log 
+sed '/^2/d' /opt/videosoft/vs-os-interface/log/vs-fast-food-2.0_2023-08-25.log >>newlog.log 
+sed '/^2/d' /opt/videosoft/vs-os-interface/log/vs-fast-food-2.0_2023-08-24.log >>newlog.log 
+sed '/^2/d' /opt/videosoft/vs-os-interface/log/vs-fast-food-2.0_2023-08-23.log >>newlog.log 
+sed '/^2/d' /opt/videosoft/vs-os-interface/log/vs-fast-food-2.0_2023-08-22.log >>newlog.log 
 sed '/^STATUS/d' newlog.log >>newlog3.log && mv newlog3.log newlog.log
 sed '/^RESPONSE/d' newlog.log >>newlog3.log && mv newlog3.log newlog.log
 sed '/^</d' newlog.log >>newlog3.log && mv newlog3.log newlog.log
@@ -16,7 +20,7 @@ grep -v '' newlog.log >>newlog3.log && mv newlog3.log newlog.log
 sed 's/^[ \t]*//' newlog.log | sed -E 'N;/^(.*)\n\1/!P;D' >>newlog3.log && mv newlog3.log newlog.log
 awk '!a[$0]++' newlog.log >>newlog3.log && mv newlog3.log newlog.log
 grep -A1 "document\|score" newlog.log >>manialogfood.log
-done
+#done
 awk '!a[$0]++' manialogfood.log >>$addr-food.log
 rm manialogfood.log
 # Envio do dos logs processados para o servidor remoto
