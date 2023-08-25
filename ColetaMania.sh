2544679b7403cf9c551bd207ce4fea817d4dbd44
Diego Meirinho C Pereira Gonçalves <137406825+diego-mpereira@users.noreply.github.com>
2023-08-25 08:13:03 -0300

    Update ColetaMania.sh

--- ColetaMania.sh
+++ ColetaMania.sh
@@ -24,7 +24,7 @@ done
 mv manialogfood.log $addr-food.log
 mv manialogself.log $addr-self.log
 # Envio do dos logs processados para o servidor remoto
-curl -u 'diego:Stonesour159!@' -T $addr-food.log --insecure sftp://diegovps.vps-kinghost.net/home/diego/manialogs/
-curl -u 'diego:Stonesour159!@' -T $addr-self.log --insecure sftp://diegovps.vps-kinghost.net/home/diego/manialogs/
+curl -u 'diego:V$V$V$3363' -T $addr-food.log --insecure sftp://diegovps.vps-kinghost.net/home/diego/manialogs/
+curl -u 'diego:V$V$V$3363' -T $addr-self.log --insecure sftp://diegovps.vps-kinghost.net/home/diego/manialogs/
 rm newlog.log && rm $addr-food.log && rm $addr-self.log
 
