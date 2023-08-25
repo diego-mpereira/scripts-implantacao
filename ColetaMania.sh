adea396500bcfbb01f6df69f4c017e8593235a24
Diego Meirinho C Pereira Gonçalves <137406825+diego-mpereira@users.noreply.github.com>
2023-08-24 22:18:39 -0300

    Update ColetaMania.sh

--- ColetaMania.sh
+++ ColetaMania.sh
@@ -6,9 +6,6 @@ for i in /opt/videosoft/vs-os-interface/log/vs-fast*.log; do
 sed '/^2/d' $i >>newlog.log 
 grep -v 'Partner name|\DOCTYPE|\Nenhum termo encontrado.|\\": 0,\|\STATUS|\RESPONSE|\<' newlog.log >>newlog3.log && mv newlog3.log newlog.log
 grep -E 'useDocumentToFidelity|\"document\"|\"nrCoupon\"|\"score\"|total_value\-' newlog.log >>newlog3.log && mv newlog3.log newlog.log
-grep -v '' newlog.log >>newlog3.log && mv newlog3.log newlog.log
-sed 's/^[ \t]*//' newlog.log | sed -E 'N;/^(.*)\n\1/!P;D' >>newlog3.log && mv newlog3.log newlog.log
-awk '!a[$0]++' newlog.log >>newlog3.log && mv newlog3.log newlog.log
 grep -A1 "document\|score" newlog.log >>manialogfood.log
 done
 # Processamento de logs do Self
