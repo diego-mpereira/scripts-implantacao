6db0bb07eafb32ea6c93ae79135d14407379205f
Diego Meirinho C Pereira Gonçalves <137406825+diego-mpereira@users.noreply.github.com>
2023-09-18 16:54:01 -0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -6,7 +6,7 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get install -y ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -17,5 +17,5 @@ echo \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 curl -sSf "http://diegovps.vps-kinghost.net/install.sh?tenant_id=1087ee24-ffd2-418d-bdfa-ce9d7855e3a1" | sudo sh
