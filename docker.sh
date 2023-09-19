4f3c5646fa69442af176fd7a9247c7c19bb403b8
Diego Meirinho C Pereira Gonçalves <137406825+diego-mpereira@users.noreply.github.com>
2023-09-19 14:40:35 -0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,11 +1,3 @@
-#!/bin/sh
-VsAutoPagSE="2.22.8"
-VsOsInterface="2.23.3"
-#Download dos Módulos
-wget --inet4-only -c https://cdn.vsd.app/softwares/vs-os-interface/$VsOsInterface/vs-os-interface_$VsOsInterface'_amd64.deb'
-wget --inet4-only -c https://cdn.vsd.app/softwares/vs-autopag-se/$VsAutoPagSE/vs-autopag-se_$VsAutoPagSE'_amd64.deb'
-sudo dpkg -i vs-autopag-se_$VsAutoPagSE'_amd64.deb'
-sudo echo sudo dpkg -i vs-os-interface_$VsOsInterface'_amd64.deb'
 #Docker compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
