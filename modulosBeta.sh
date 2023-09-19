ed083977dad491cf70a81fe9ca9bf4c9aa8815b7
Diego Meirinho C Pereira Gonçalves <137406825+diego-mpereira@users.noreply.github.com>
2023-09-19 14:41:14 -0300

    Update modulosBeta.sh

--- modulosBeta.sh
+++ modulosBeta.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 VsAutoPagSE="2.22.8"
 VsOsInterface="2.23.3"
-#Download dos Módulos
 wget --inet4-only -c https://cdn.vsd.app/softwares/vs-os-interface/$VsOsInterface/vs-os-interface_$VsOsInterface'_amd64.deb'
 wget --inet4-only -c https://cdn.vsd.app/softwares/vs-autopag-se/$VsAutoPagSE/vs-autopag-se_$VsAutoPagSE'_amd64.deb'
 sudo dpkg -i vs-autopag-se_$VsAutoPagSE'_amd64.deb'
