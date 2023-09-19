9c9f2da2ced159571c8db9f32e58d763c3de0f87
Diego Meirinho C Pereira Gonçalves <137406825+diego-mpereira@users.noreply.github.com>
2023-09-19 14:59:07 -0300

    Update modulosBeta.sh

--- modulosBeta.sh
+++ modulosBeta.sh
@@ -4,5 +4,5 @@ VsOsInterface="2.23.3"
 wget --inet4-only -c https://cdn.vsd.app/softwares/vs-os-interface/$VsOsInterface/vs-os-interface_$VsOsInterface'_amd64.deb'
 wget --inet4-only -c https://cdn.vsd.app/softwares/vs-autopag-se/$VsAutoPagSE/vs-autopag-se_$VsAutoPagSE'_amd64.deb'
 sudo dpkg -i vs-autopag-se_$VsAutoPagSE'_amd64.deb'
-sudo echo sudo dpkg -i vs-os-interface_$VsOsInterface'_amd64.deb'
+sudo dpkg -i vs-os-interface_$VsOsInterface'_amd64.deb'
 sudo wget -O- https://raw.githubusercontent.com/diego-mpereira/scripts-implantacao/main/docker.sh | sudo sh
