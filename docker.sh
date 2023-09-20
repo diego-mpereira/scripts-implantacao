7b7958c5908593eb372b7140352c4d258c5e0845
Diego Meirinho C Pereira Gonçalves <137406825+diego-mpereira@users.noreply.github.com>
2023-09-19 21:55:29 -0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -4,7 +4,7 @@ sudo chmod +x /usr/local/bin/docker-compose
 sudo apt-get update
 sudo apt-get install -y ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 sudo cp /etc/apt/trusted.gpg /etc/apt/trusted.gpg.d/trusted2.gpg
 echo \
