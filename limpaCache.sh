ae7334446c2cc46060ad9b2a57c51203b3383703
Diego Meirinho C Pereira Gonçalves <137406825+diego-mpereira@users.noreply.github.com>
2023-10-16 15:51:05 -0300

    Update limpaCache.sh

--- limpaCache.sh
+++ limpaCache.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo rm -r .cache/google-chrome*/* 
 sudo rm -r .config/google-chrome*/*
-sudo cd /opt/videosoft/vs-autopag-se/
+cd /opt/videosoft/vs-autopag-se/
 sudo service vs-autopag-se stop
 sudo timeout 40 node/bin/node vs-autopag-se.js
 sudo service vs-autopag-se start
